abe360eeb53ceb4236a080be6503fdda4f5e6ba3
Kiril Hansen <kiril@snabelkat.dk>
2025-04-17 08:39:49 +0200

    Added 4 new items
    clump_of_gilded_blackstone_rocks
    gilded_blackstone_rock
    smelted_template_base
    template_base

--- src/main/java/net/kiril/advancedupgradetemplates/items/ModItems.java
+++ src/main/java/net/kiril/advancedupgradetemplates/items/ModItems.java
@@ -15,6 +15,10 @@ import java.util.function.Function;
 public class ModItems {
 
     public static final Item NETHERRACK_ROCK = registerItem("netherrack_rock", Item::new, new Item.Settings());
+    public static final Item GILDED_BLACKSTONE_ROCK = registerItem("gilded_blackstone_rock", Item::new, new Item.Settings());
+    public static final Item TEMPLATE_BASE = registerItem("template_base", Item::new, new Item.Settings());
+    public static final Item SMELTED_TEMPLATE_BASE = registerItem("smelted_template_base", Item::new, new Item.Settings());
+    public static final Item CLUMP_GILDED_BLACKSTONE_ROCKS = registerItem("clump_of_gilded_blackstone_rocks", Item::new, new Item.Settings());
 
     public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registerKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(AdvancedUpgradeTemplatesMod.MOD_ID, name));
@@ -22,10 +26,16 @@ public class ModItems {
     }
 
     private static void customIngredients(FabricItemGroupEntries entries) {
+
         entries.add(NETHERRACK_ROCK);
+        entries.add(GILDED_BLACKSTONE_ROCK);
+        entries.add(TEMPLATE_BASE);
+        entries.add(SMELTED_TEMPLATE_BASE);
+        entries.add(CLUMP_GILDED_BLACKSTONE_ROCKS);
     }
 
     public static void registerModItems() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::customIngredients);
     }
 }
+
