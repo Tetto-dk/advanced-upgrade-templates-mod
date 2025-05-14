587d842b15cc6cd142e2dc468c2ea8da06afd2a4
Kiril Hansen <kiril@snabelkat.dk>
2025-05-14 16:27:47 +0200

    Added Tooltip

--- advanced-upgrade-templates-mod-1.20.1/src/main/java/net/tetto/advancedupgradetemplates/items/ModItems.java
+++ advanced-upgrade-templates-mod-1.20.1/src/main/java/net/tetto/advancedupgradetemplates/items/ModItems.java
@@ -3,13 +3,19 @@ package net.tetto.advancedupgradetemplates.items;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.Rarity;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import net.tetto.advancedupgradetemplates.AdvancedUpgradeTemplatesMod;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 
 public class ModItems {
@@ -19,10 +25,54 @@ public class ModItems {
     public static final Item TEMPLATE_BASE = registerItem("template_base", new Item(new FabricItemSettings()));
     public static final Item SMELTED_TEMPLATE_BASE = registerItem("smelted_template_base", new Item(new FabricItemSettings()));
     public static final Item CLUMP_GILDED_BLACKSTONE_ROCKS = registerItem("clump_of_gilded_blackstone_rocks", new Item(new FabricItemSettings()));
-    public static final Item NETHERITE_DIAMOND_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_diamond_upgrade_smithing_template", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
-    public static final Item NETHERITE_EMERALD_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_emerald_upgrade_smithing_template", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
-    public static final Item NETHERITE_GOLD_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_gold_upgrade_smithing_template", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
-    public static final Item NETHERITE_IRON_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_iron_upgrade_smithing_template", new Item(new FabricItemSettings().rarity(Rarity.UNCOMMON)));
+    public static final Item NETHERITE_DIAMOND_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_diamond_upgrade_smithing_template", new Item(new FabricItemSettings()){
+        @Override
+        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_diamond_upgrade_smithing_template.tooltip"));
+            super.appendTooltip(stack, world, tooltip, context);
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.blank_line"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_diamond_upgrade_smithing_template.applies_to"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_diamond_upgrade_smithing_template.armor_type"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_diamond_upgrade_smithing_template.ingredients"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_diamond_upgrade_smithing_template.ingredient"));
+        }
+    });
+    public static final Item NETHERITE_EMERALD_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_emerald_upgrade_smithing_template", new Item(new FabricItemSettings()){
+        @Override
+        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_emerald_upgrade_smithing_template.tooltip"));
+            super.appendTooltip(stack, world, tooltip, context);
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.blank_line"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_emerald_upgrade_smithing_template.applies_to"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_emerald_upgrade_smithing_template.armor_type"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_emerald_upgrade_smithing_template.ingredients"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_emerald_upgrade_smithing_template.ingredient"));
+        }
+    });
+    public static final Item NETHERITE_GOLD_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_gold_upgrade_smithing_template", new Item(new FabricItemSettings()){
+        @Override
+        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_gold_upgrade_smithing_template.tooltip"));
+            super.appendTooltip(stack, world, tooltip, context);
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.blank_line"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_gold_upgrade_smithing_template.applies_to"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_gold_upgrade_smithing_template.armor_type"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_gold_upgrade_smithing_template.ingredients"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_gold_upgrade_smithing_template.ingredient"));
+        }
+    });
+    public static final Item NETHERITE_IRON_UPGRADE_SMITHING_TEMPLATE = registerItem("netherite_iron_upgrade_smithing_template", new Item(new FabricItemSettings()){
+        @Override
+        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_iron_upgrade_smithing_template.tooltip"));
+            super.appendTooltip(stack, world, tooltip, context);
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.blank_line"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_iron_upgrade_smithing_template.applies_to"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_iron_upgrade_smithing_template.armor_type"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_iron_upgrade_smithing_template.ingredients"));
+            tooltip.add(Text.translatable("item.advancedupgradetemplatesmod.netherite_iron_upgrade_smithing_template.ingredient"));
+        }
+    });
 
 
 
